576cc84ecdcf4a29c58210a3cf3699ec88469591
locutus <ryan@molecke.com>
2014-10-28 07:00:38 -0600

    README.md initialized, CMake build, clang scripts, build test passed.

---
+++ src/main/NURBSurfer.cpp
@@ -0,0 +1,24 @@
+/**************************
+*   C++ Project Template by Ryan Molecke, Ph.D.
+*   Tuesday, October 28th, 2014
+*
+*   includes simple CMake build system
+*   includes clang scripts
+*
+*    to build test: from top folder
+*       mkdir build
+*       cmake ..
+*       make
+*
+***************************/
+
+// memory include to test C++11 build
+#include <memory>
+#include <iostream>
+
+int main(int argc, char **argv){
+
+    std::cout
+            << "¸.·´¯`·.¸¸.·´ NURBSurfer - Build Test Passed `·.¸¸.·´¯`·.¸\n";
+
+}
